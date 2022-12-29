250ea2e0a1a4fc35fb2f61e3beee0ad7003b5369
GFGJewbacca <121557221+GFGJewbacca@users.noreply.github.com>
2022-12-29 16:47:14 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Import stylesheets
 import './style.css';
-import { songItem } from './songItem.js';
+import { songItem, inventory, shopCart } from './shopCart.js';
 
 // Write Javascript code!
+let shoppingcart;
+shoppingcart = new shopCart('shoppingcart');
+let item = shoppingcart.search('Oseh_Shalom');
+shoppingcart.addToCart(item);
+shoppingcart.removeFromCart(item);
 
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = ``;
+appDiv.innerHTML = shoppingcart.cart[0].name;
--- shopCart.js
+++ shopCart.js
@@ -1,10 +1,42 @@
 import { inventory, songItem } from './inventory.js';
-let search = 0;
-let cart = [];
-search = (name) => {
-  let i = 0;
-  while (name != inventory[i].name) {
-    i++;
+class shopCart {
+  constructor(id) {
+    this.cart = [];
+    this.id = id;
   }
-  return i;
-};
+
+  search(name) {
+    let i = 0;
+    while (name != inventory[i].name) {
+      i++;
+    }
+    return i;
+  }
+
+  addToCart(item) {
+    this.cart[this.cart.length] = inventory[item];
+  }
+
+  removeFromCart(item) {
+    let i = 0;
+    while (item.name != this.cart[i].name) {
+      i++;
+    }
+    switch (i) {
+      case 0:
+        this.cart.shift();
+        break;
+
+      case this.cart.length:
+        this.cart.pop();
+        break;
+
+      default:
+        const beforeItem = this.cart.splice(0, i);
+        const afterItem = this.cart.splice(i + 1, this.cart.length);
+        this.cart = beforeItem.concat(afterItem);
+        break;
+    }
+  }
+}
+export { shopCart, inventory, songItem };
