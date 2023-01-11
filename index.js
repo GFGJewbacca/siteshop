ac3ad053edd3b8f41436fcdd06ff36566ad77e56
GFGJewbacca <121557221+GFGJewbacca@users.noreply.github.com>
2023-01-11 11:12:07 +0200

    3 files modified, 2 added

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 // Import stylesheets
 import './style.css';
-import { shopCart } from './shopCart.js';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js';
+import "./modal.js"
+import "./shopScript.js"
 
 // Write Javascript code!
-const shopper = new shopCart();
-shopper.addToCart('Oseh_Shalom');
-shopper.addToCart(`L'cha_Dodi`);
-document.getElementById('display').innerHTML =
-  '<table>' + shopper.displayCart();
-+'</table>';
