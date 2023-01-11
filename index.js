a74d34754e9f7167346f1fcf03787700f26d0f11
GFGJewbacca <121557221+GFGJewbacca@users.noreply.github.com>
2023-01-11 14:28:30 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Import stylesheets
 import './style.css';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js';
-import "./modal.js"
-import "./shopScript.js"
+import './modal.js';
+import './shopScript.js';
 
 // Write Javascript code!
